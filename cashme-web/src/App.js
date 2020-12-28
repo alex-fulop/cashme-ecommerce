66236c0555d14554975a0c9c663b695b00e7ef22
Fulop Alex-Dominic <fulopalex2000@gmail.com>
2020-12-29 01:05:49 +0200

    converted from static web app to react, and added new products

--- cashme-web/src/App.js
+++ cashme-web/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import data from "./data";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header className="row">
+        <div className="branding">
+          <a href="index.html">cashme</a>
+        </div>
+        <nav>
+          <ul className="navigation">
+            <li><a href="cart.html">cart</a></li>
+            <li><a href="signin.html">sign in</a></li>
+          </ul>
+        </nav>
       </header>
+      <main>
+        <div className="row center">
+          {
+            data.products.map((product) => (
+              <div key={product._id} className="card">
+                <a href={`/product/${product._id}`}>
+                  <img className="medium" src={product.image} alt="product"></img>
+                </a>
+                <div className="card-body">
+                  <a href={`/product/${product._id}`}>
+                    <h2 className="upper-case">{product.name}</h2>
+                  </a>
+                </div>
+                <div className="rating">
+                  <span><i className="fa fa-star"></i></span>
+                  <span><i className="fa fa-star"></i></span>
+                  <span><i className="fa fa-star"></i></span>
+                  <span><i className="fa fa-star"></i></span>
+                  <span><i className="fa fa-star"></i></span>
+                </div>
+                <div className="price">{product.price}$</div>
+              </div>
+            ))
+          }
+        </div>
+      </main>
+      <footer className="row center">
+        Copyright &copy; cashme 2020. All rights reserved.
+        </footer>
     </div>
   );
 }
---
+++ cashme-web/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products: [
+        {
+            _id: "1",
+            name: "Air Jordan 1 Retro High Union Los Angeles Black Toe",
+            category: "Sneakers",
+            image: "/images/product-1.jpeg",
+            price: 1910,
+            brand: "Nike",
+            rating: 4.5,
+            numReviews: 10,
+            description: "AJ 1 High, DS Retro"
+        },
+        {
+            _id: "2",
+            name: "Air Jordan 1 Retro High Union Los Angeles Blue Toe",
+            category: "Sneakers",
+            image: "/images/product-2.jpeg",
+            price: 2250,
+            brand: "Nike",
+            rating: 4.7,
+            numReviews: 12,
+            description: "AJ 1 High, DS Retro"
+        },
+        {
+            _id: "3",
+            name: "Air Max 1 \"Have a nike day\"",
+            category: "Sneakers",
+            image: "/images/product-3.jpeg",
+            price: 154,
+            brand: "Nike",
+            rating: 4.2,
+            numReviews: 8,
+            description: "AM 1 Mid, Space Purple/Black-Bleached Coral"
+        },
+        {
+            _id: "4",
+            name: "Jordan 12 Retro Reverse Flu Game",
+            category: "Sneakers",
+            image: "/images/product-4.jpeg",
+            price: 700,
+            brand: "Nike",
+            rating: 4.5,
+            numReviews: 10,
+            description: "J12 High, Varsity Red/Black"
+        },
+        {
+            _id: "5",
+            name: "New Balance 996 Grey Navy",
+            category: "Sneakers",
+            image: "/images/product-5.jpeg",
+            price: 543,
+            brand: "New Balance",
+            rating: 4.1,
+            numReviews: 77,
+            description: "NB 996, Black/Eclipse"
+        },
+        {
+            _id: "6",
+            name: "Nike Air Max 95 OG Neon (2020)",
+            category: "Sneakers",
+            image: "/images/product-6.jpeg",
+            price: 270,
+            brand: "Nike",
+            rating: 4.9,
+            numReviews: 13,
+            description: "AM 95 OG, Black Neon Yellow-Light Graphite"
+        }
+    ]
+}
+
+export default data;
